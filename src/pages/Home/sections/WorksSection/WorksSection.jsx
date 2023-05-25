c7a46a8e279365643d4e85cd7dd11bd966ce24f6
Severuna <oxxxyfomenkova@gmail.com>
2023-05-25 22:28:29 +0300

    use reducer works in worksection home

--- src/pages/Home/sections/WorksSection/WorksSection.jsx
+++ src/pages/Home/sections/WorksSection/WorksSection.jsx
@@ -3,52 +3,18 @@ import Button from '../../../module/Button/Button';
 import Section from '../../../module/Section/Section';
 import ListContainer from '../../../module/ListContainer/ListContainer';
 import WorkItem from './WorkItem/WorkItem';
-import accessories from './img/Accessories.png';
-import vases from './img/vases.png';
-import biofireplaces from './img/Biofireplaces.png';
-import aroma_lamps from './img/aroma_lamps.png';
-import light from './img/light.png';
-import interior from './img/interior.png';
+import { useSelector } from 'react-redux';
 
 const WorksSection = () => {
 
-    const works_list = [
-        {
-            id: 0,
-            src: vases,
-            title: 'vases'
-        },
-        {
-            id: 1,
-            src: accessories,
-            title: 'accessories'
-        },
-        {
-            id: 2,
-            src: light,
-            title: 'light'
-        },
-        {
-            id: 3,
-            src: aroma_lamps,
-            title: 'aroma lamps'
-        },
-        {
-            id: 4,
-            src: biofireplaces,
-            title: 'biofireplaces'
-        },
-        {
-            id: 5,
-            src: interior,
-            title: 'in the interior'
-        },
-    ];
+    const WORKS_LIST = useSelector((state) => state.works);
 
     let elements = [];
     
-    works_list.forEach((element) => {
-        elements.push(<WorkItem key={element.id} {...element} />)
+    WORKS_LIST.forEach((element) => {
+        if (element.id <= 5) {
+            elements.push(<WorkItem key={element.id} {...element} />)
+        }
     })
 
     return (
