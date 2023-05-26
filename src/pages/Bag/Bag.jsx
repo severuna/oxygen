1f2d710902d339b97a89b47226462b330073ce28
Severuna <oxxxyfomenkova@gmail.com>
2023-05-26 19:09:50 +0300

    add bag page content

--- src/pages/Bag/Bag.jsx
+++ src/pages/Bag/Bag.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import './Bag.css';
+import Section from '../module/Section/Section';
+import ListContainer from '../module/ListContainer/ListContainer';
+import WorkItem from '../Works/Work/Work';
+import { useSelector } from 'react-redux';
 
 const Bag = () => {
+
+    const ORDER_LIST = useSelector((state) => state.orders);
+
+    let elements = [];
+
+    if(Object.keys(ORDER_LIST).length === 0 ) {
+
+        elements = <h2 className='title'>Your order list is empty.</h2>
+
+    } else {
+
+        ORDER_LIST.orders.forEach((element) => {
+            
+            elements.push(<WorkItem key={element.order.id + Math.random()} {...element.order}/>);
+
+        });
+    }
+
     return (
-        <div>
-            <h1>Bag</h1>
-        </div>
+        <Section content={ 
+            <ListContainer content={elements}/>
+            }
+        />
     );
 };
 
